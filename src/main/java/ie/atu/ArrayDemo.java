a6ba8beff7d8bc62f3a9d3b99cff219b00e7f866
BenMcCormack <g00416547@atu.ie>
2023-11-06 10:59:32 +0000

    Stuck on inputs

--- src/main/java/ie/atu/ArrayDemo.java
+++ src/main/java/ie/atu/ArrayDemo.java
@@ -1,9 +1,12 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class ArrayDemo
 {
+    Scanner scanner = new Scanner(System.in);
     public static void main(String[] args)
     {
         int x;
@@ -15,5 +18,12 @@ public class ArrayDemo
             anArray[x] = x*100+100;
             System.out.println("Element at index " + x + ": " + anArray[x]);
         }
+
+        System.out.println("Please enter the next variable: ");
+        anArray[10] = Inpuq(scanner.nextInt());
+    }
+    int Inpuq(int q)
+    {
+        return q;
     }
 }
