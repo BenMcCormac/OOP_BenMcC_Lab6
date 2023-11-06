8e61e13aab32c48afe46b345eb6277fe8fa2cc5c
BenMcCormack <g00416547@atu.ie>
2023-11-06 10:18:21 +0000

    Start

---
+++ src/main/java/ie/atu/ArrayDemo.java
@@ -0,0 +1,9 @@
+package ie.atu;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class ArrayDemo {
+    public static void main(String[] args) {
+
+    }
+}
